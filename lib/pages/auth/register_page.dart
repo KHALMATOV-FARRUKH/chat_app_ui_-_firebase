f40966a7b0e34a82cc4002c2b185320b3da717c7
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-02-14 21:32:24 +0500

    Designed the register page
    Created dart file:
    register_page.dart

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,19 +1,20 @@
-import 'package:chat_app_ui_2/pages/auth/register_page.dart';
+import 'package:chat_app_ui_2/pages/auth/login_page.dart';
 import 'package:chat_app_ui_2/widgets/widgets.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<RegisterPage> createState() => _RegisterPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _RegisterPageState extends State<RegisterPage> {
   final formKey = GlobalKey<FormState>();
   String email = "";
   String password = "";
+  String fullName = "";
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +37,32 @@ class _LoginPageState extends State<LoginPage> {
                 ),
                 const SizedBox(height: 10),
                 const Text(
-                  "Login now to see what they are talking",
+                  "Create your account now to chat and explore",
                   style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
                 ),
-                Image.asset("assets/login.png"),
+                Image.asset("assets/register.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                    labelText: "Full Name",
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+                  validator: (val) {
+                    if (val!.isNotEmpty) {
+                      return null;
+                    } else {
+                      return "Name cannot be empty ";
+                    }
+                  },
+                ),
+                SizedBox(height: 15),
                 TextFormField(
                   decoration: textInputDecoration.copyWith(
                     labelText: "Email",
@@ -96,10 +119,10 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     onPressed: () {
-                      login();
+                      register();
                     },
                     child: const Text(
-                      "Sign In",
+                      "Register",
                       style: TextStyle(color: Colors.white, fontSize: 16),
                     ),
                   ),
@@ -107,17 +130,17 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 10),
                 Text.rich(
                   TextSpan(
-                      text: "Don't have an account? ",
+                      text: "Already have an account? ",
                       style: TextStyle(color: Colors.black, fontSize: 14),
                       children: <TextSpan>[
                         TextSpan(
-                            text: "Register here",
+                            text: "Login now",
                             style: const TextStyle(
                                 color: Colors.black,
                                 decoration: TextDecoration.underline),
                             recognizer: TapGestureRecognizer()
                               ..onTap = () {
-                                nextScreenReplace(context, RegisterPage());
+                                nextScreenReplace(context, LoginPage());
                               }),
                       ]),
                 ),
@@ -129,5 +152,5 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  login() {}
+  register() {}
 }
