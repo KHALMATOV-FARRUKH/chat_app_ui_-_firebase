05dcf4ac6415d9013dd732b6b804f4e10d565520
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2023-02-14 17:42:58 +0500

    Added plugin: com.google.gms.google-services
    Added dependence: classpath 'com.google.gms:google-services:4.3.15'
    
    Created dart files:
    constants.dart
    helper_function.dart
    home_page.dart
    login_page.dart
    
    Added google-services.json
    
    Added dependence:
    cloud_firestore: ^4.3.2
    firebase_auth: ^4.2.6
    firebase_core: ^2.5.0
    shared_preferences: ^2.0.15

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,58 @@
+import 'package:chat_app_ui_2/helper/helper_function.dart';
+import 'package:chat_app_ui_2/pages/home_page.dart';
+import 'package:chat_app_ui_2/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+    // run the initialization for web
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messagingSenderId,
+            projectId: Constants.projectId));
+  }else{
+    // run the initialization for android, ios
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if(value != null){
+        _isSignedIn = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? HomePage() : LoginPage(),
     );
   }
 }
---
+++ lib/shared/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static String appId = "1:450013683466:web:19aa15437005b6a9af64f3";
+  static String apiKey = "AIzaSyBdTJwWp_6U8YbnO4Z73kO1QRIzrLhnv7U";
+  static String messagingSenderId = "450013683466";
+  static String projectId = "chatappflutter-b8ba3";
+
+
+}
